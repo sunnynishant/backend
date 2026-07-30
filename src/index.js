5d6486a5c838c9a4cb94679e415ded8e3f192bd6
sunnythakurdtu <sunnythakur_se20b9_31@dtu.ac.in>
2026-07-30 11:56:25 +0530

    apiresponse,error,asynchandler

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-
+import app from "./app.js"
 dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running on port:${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+
+})
 /*import express from "express";
 const app=express();
 (async()=>{
